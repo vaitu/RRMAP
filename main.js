5f1eb5adb20d511b55e7a16bcf6dbb28e8c69c2d
vaitu <yf.21.stf@gmail.com>
2016-07-30 11:48:43 +0900

    開始点の変更

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
   viewer.dataSources.add(Cesium.KmlDataSource.load("restroom.kmz"));
 
 viewer.camera.setView({
-  destination: Cesium.Cartesian3.fromDegrees(138, 30, 3000000),
+  destination: Cesium.Cartesian3.fromDegrees(138, 30, 300),
   orientation: {
     heading: 0, // 水平方向の回転度
     pitch: -1.3, // 垂直方向の回転度
